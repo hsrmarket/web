94281418b2a15e4d26bb0a86e6895852fddcb634
Elias Brunner <elias_brunner@gmx.ch>
2017-04-01 14:45:04 +0200

    changed pretty much everything
    deleted bookname property from model and make it suitable for the rest
    api

--- controller/appController-compiled.js
+++ controller/appController-compiled.js
@@ -1,12 +1,12 @@
 var appService = require("../services/appService");
 
-module.exports.showHome = function(req, res) {
+module.exports.showHome = function (req, res) {
     res.render('index');
 };
 
 module.exports.getArticle = function (req, res) {
-    appService.getAll(function(err, books) {
-        res.render('currentCollection', {books : books});
+    appService.getAll(function (err, books) {
+        res.render('currentCollection', { books: books });
     });
 };
 
@@ -16,8 +16,9 @@ module.exports.createArticle = function (req, res) {
 
 module.exports.postArticle = function (req, res) {
 
-    var book = appService.add(req.body.id, req.body.iban, req.body.author, function(err, book) {
+    var book = appService.add(req.body.id, req.body.iban, req.body.author, function (err, book) {
         res.render('success', book);
     });
-
 };
+
+//# sourceMappingURL=appController-compiled.js.map
--- services/appService-compiled.js
+++ services/appService-compiled.js
@@ -1,18 +1,16 @@
 var Datastore = require('nedb');
 var db = new Datastore({ filename: '../data/list.db', autoload: true });
 
-function Book(id, iban, author)
-{
+function Book(id, iban, author) {
     this.id = id;
     this.iban = iban;
     this.author = author;
 }
 
-
-function insertArticle(id, iban, author , callback) {
+function insertArticle(id, iban, author, callback) {
     var book = new Book(id, iban, author);
-    db.insert(book, function(err, newDoc){
-        if(callback){
+    db.insert(book, function (err, newDoc) {
+        if (callback) {
             callback(err, newDoc);
         }
     });
@@ -28,3 +26,5 @@ module.exports = {
     add: insertArticle,
     getAll: getAllArticle
 };
+
+//# sourceMappingURL=appService-compiled.js.map
