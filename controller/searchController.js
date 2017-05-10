934cfe0f9f59b86733e23b31dbebbe0ac7e3b945
Urs Forrer <mail@ursforrer.ch>
2017-05-10 15:48:01 +0200

    Bug Fixing
    Bug Fixing

--- controller/searchController.js
+++ controller/searchController.js
@@ -3,13 +3,18 @@
  */
 
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var request = require('request');
 
-var URL = "http://duernten.forrer.network:9000/api/search";
+var URL = "http://rest.hsrmarket.ch:9000/api/search";
 
 /* Accounts */
 
 module.exports.searchByKey = function (req, res) {
-    let searchKey = req.body.search;
+    var searchKey = req.query.search;
+    if (searchKey == null || searchKey == "") {
+        // Redirect if no parameter is send
+        res.redirect("/");
+    }
     var options = {
         uri: URL,
         method: 'POST',
@@ -20,8 +25,7 @@ module.exports.searchByKey = function (req, res) {
 
     request(options, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            console.log(body);
-            response.render("articleList", { search: true , articles : body});
+            res.render("articleList", { search: true , articles : body, username : req.session.username, isadmin : req.session.isadmin});
         } else {
             console.log(body);
         }
--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -4,6 +4,6 @@ var express = require('express');
 var router = express.Router();
 var searchController = require('../controller/searchController');
 
-router.post('/', searchController.searchByKey);
+router.get('/', searchController.searchByKey);
 
 module.exports = router;
