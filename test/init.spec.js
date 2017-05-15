659a71b14b157ceaddf9cd2f868b37269848764d
Urs Forrer <mail@ursforrer.ch>
2017-05-15 10:42:01 +0200

    Deleted wrong tests
    Fixing the test

--- test/init.spec.js
+++ test/init.spec.js
@@ -3,31 +3,3 @@
  */
 
 let request = require('request');
-
-describe("HSRmarket nodejs server spec",function () {
-
-    describe("/GET article with the id 59,", function () {
-        it("then the article should have the id 59", function (done) {
-
-            const result = JSON.stringify({
-                "id": 59,
-                "name": "JUnitElectronic",
-                "price": 15.0,
-                "condition": 5,
-                "description": "This electronic was created with the JUnit Test",
-                "creationDate": "2017-04-21",
-                "image": "",
-                "type": "electronic",
-                "producer": "JUnit",
-                "model": "JUnit"
-            });
-
-            request("http://duernten.forrer.network:9000/api/articles/59", function (error, response, body) {
-                expect(body).toEqual(result);
-                done();
-            });
-        });
-    });
-
-
-});
