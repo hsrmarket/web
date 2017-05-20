d348babe01d52cd579637ff1619ee1df827176c8
Elias Brunner <elias_brunner@gmx.ch>
2017-05-20 14:29:20 +0200

    refactored

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,15 +2,15 @@
  * Created by felix_2 on 03.05.2017.
  */
 
-var express = require('express');
-var router = express.Router();
-var userController = require('../controller/userController');
-var util = require('../util/security');
+const express = require('express');
+const router = express.Router();
+const userController = require('../controller/userController');
+const util = require('../util/security');
 
 router.all('/*', util.handleAuthenticate);
-router.get("/", userController.getMyAccount);
-router.get("/articles", userController.getMyArticle);
-router.get("/sales", userController.getMySales);
-router.get("/purchases", userController.getMyPurchases);
+router.get('/', userController.getMyAccount);
+router.get('/articles', userController.getMyArticle);
+router.get('/sales', userController.getMySales);
+router.get('/purchases', userController.getMyPurchases);
 
 module.exports = router;
