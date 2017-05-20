9c877e9fa1f1f54f7e57869808379b0ec8b5e599
Elias Brunner <elias_brunner@gmx.ch>
2017-05-20 14:09:13 +0200

    refactored

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -2,18 +2,18 @@
  * Created by felix_2 on 03.05.2017.
  */
 
-var express = require('express');
-var router = express.Router();
-var accountController = require('../controller/accountController');
-var util = require('../util/security');
+const express = require('express');
+const router = express.Router();
+const accountController = require('../controller/accountController');
+const util = require('../util/security');
 
 router.all('/*', util.handleAuthenticate);
 
 /* Account*/
 
-router.get("/:id/edit", accountController.getAccountDetailsByID);
-router.post("/:id",accountController.saveAccountToDB);
-router.get("/:id/delete", accountController.deleteAccount);
+router.get('/:id/edit', accountController.getAccountDetailsByID);
+router.post('/:id', accountController.saveAccountToDB);
+router.get('/:id/deconste', accountController.deconsteAccount);
 
 
 module.exports = router;
