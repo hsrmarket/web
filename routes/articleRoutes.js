1f5d40a74408268bb488cc7577dab292542b3e59
Elias Brunner <elias_brunner@gmx.ch>
2017-05-20 14:11:59 +0200

    refactored

--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -1,28 +1,25 @@
-'use strict';
+const express = require('express');
+const router = express.Router();
+const appController = require('../controller/articleController');
+const util = require('../util/security');
 
-var express = require('express');
-var router = express.Router();
-var appController = require('../controller/articleController');
-var util = require('../util/security');
-
-router.get("/books", appController.getArticles);
-router.get("/electronics", appController.getArticles);
-router.get("/officesupplies", appController.getArticles);
-router.get("/other", appController.getArticles);
-router.get("/recent", appController.getArticles);
-router.get("/padd", appController.getPreSelectionPage);
-router.get("/add", appController.getAddPage);
-router.post("/add", appController.addArticle);
-router.get("/:id", appController.getArticlesByID);
+router.get('/books', appController.getArticles);
+router.get('/electronics', appController.getArticles);
+router.get('/officesupplies', appController.getArticles);
+router.get('/other', appController.getArticles);
+router.get('/recent', appController.getArticles);
+router.get('/padd', appController.getPreSelectionPage);
+router.get('/add', appController.getAddPage);
+router.post('/add', appController.addArticle);
+router.get('/:id', appController.getArticlesByID);
 
 router.all('/:id/edit', util.handleAuthenticate);
-router.get("/:id/delete", appController.deleteArticle);
-router.get("/:id/edit", appController.editArticleByID);
-router.post("/:id/edit",appController.saveArticleToDB);
+router.get('/:id/delete', appController.deleteArticle);
+router.get('/:id/edit', appController.editArticleByID);
+router.post('/:id/edit', appController.saveArticleToDB);
 
-//router.all('/add', util.handleAuthenticate);
-//router.all('/hello', util.handleAuthenticate);
-
+// router.all('/add', util.handleAuthenticate);
+// router.all('/hello', util.handleAuthenticate);
 
 
 module.exports = router;
